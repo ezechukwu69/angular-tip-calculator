eca756d2e38fb4612f9ed289b432f6d6de7f2340
Ojukwu Ezechukwu <ezechukwu69@gmail.com>
2021-08-26 13:59:24 +0100

    Service worker completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from 'src/environments/environment.prod';
 import { AppSummaryComponent } from './app-summary/app-summary.component';
 import { AppComponent } from './app.component';
 import { RightAlignTextComponent } from './components/right-align-text/right-align-text.component';
 import { SummaryTitleComponent } from './components/summary-title/summary-title.component';
 import { InputsComponent } from './inputs/inputs.component';
 import { CalculatorService } from './services/calculator.service';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -20,6 +21,7 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
