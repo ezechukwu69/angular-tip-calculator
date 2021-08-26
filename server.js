583e44251e3847dde578e6617b492bdb2a782f96
Ojukwu Ezechukwu <ezechukwu69@gmail.com>
2021-08-26 15:25:21 +0100

    Netlify setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+
+app.use(requireHTTPS);
+
+app.use(express.static("dist"));
+
+app.get("/", function (req, res) {
+  console.log("Getting file");
+  res.sendFile("index.html", { root: "dist/" });
+});
+
+app.listen(process.env.PORT || 8080);
